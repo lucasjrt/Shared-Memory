ceda4b25af1d40f8bbe984c9135c8a610b9645f4
Lucasjrt <lucasjrt@outlook.com>
2018-05-26 15:16:55 -0400

    Correção de bug que deslogava o usuário ou não

--- init.c
+++ init.c
@@ -39,10 +39,10 @@ int enfileira_f1() {
 		sem_post(&shared_memory->mutex);
 		return 1;
 	} else if ((shared_memory->tam == 10 && shared_memory->ctl) || (shared_memory->tam > 0 && !shared_memory->ctl)) { //se a fila estiver cheia e ainda não foi definida como esvaziando
-		printf("Solicitando a fila esvaziar: %d\n", shared_memory->tam);
+		printf("Processo %d solicitando a fila esvaziar: %d\n", getpid(), shared_memory->tam);
 		shared_memory->ctl = 0; //define a fila como esvaziando
-		kill(shared_memory->vetor[15], SIGUSR1);
 		sem_post(&shared_memory->mutex);
+		kill(shared_memory->pids[3], SIGUSR1);
 		return -1;
 	} else if (shared_memory->tam == 0 && shared_memory->ctl){ // se a fila estiver vazia e ainda não foi definida como enchendo
 		shared_memory->ctl = 1;
@@ -57,24 +57,26 @@ int enfileira_f1() {
 
 //retorna 1 se suscesso, -1 se a fila estiver vazia
 int desenfileira_f1() {
-	sem_wait(&shared_memory->mutex);
 	int i, valor;
-	printf("Processo de pid %d desenfileirando em f1, tamanho da fila: %d\n", getpid(), shared_memory->tam);
-	valor = shared_memory->vetor[0];
-	printf("Removendo o valor %d da fila\n", valor);
-	printf("Valor da posicao 4 da fila: %d\n", shared_memory->vetor[3]);
-	printf("Vetor em %d: {", getpid());
-	for(i = 0; i < shared_memory->tam - 1; i++) {
-		shared_memory->vetor[i] = shared_memory->vetor[i+1];
+	if(shared_memory->tam > 0) {
+		printf("Processo de pid %d desenfileirando em f1, tamanho da fila: %d\n", getpid(), shared_memory->tam);
+		valor = shared_memory->vetor[0];
+		printf("Removendo o valor %d da fila\n", valor);
+		printf("Valor da posicao 4 da fila: %d\n", shared_memory->vetor[3]);
+		printf("Vetor em %d: {", getpid());
+		for(i = 0; i < shared_memory->tam - 1; i++) {
+			shared_memory->vetor[i] = shared_memory->vetor[i+1];
+		}
+		shared_memory->tam --;
+		for(i = 0; i < shared_memory->tam; i++) {
+			printf("[%d] = %d",i , shared_memory->vetor[i]);
+			if(i = shared_memory->tam - 1) printf(", ");
+		}
+		printf("}\n");
+		return valor;
 	}
-	shared_memory->tam --;
-	for(i = 0; i < shared_memory->tam; i++) {
-		printf("[%d] = %d",i , shared_memory->vetor[i]);
-		if(i = shared_memory->tam - 1) printf(", ");
-	}
-	printf("}\n");
-	sem_post(&shared_memory->mutex);
-	return valor;
+	printf("Tam: %d\n", shared_memory->tam);
+	return -1;
 }
 
 void consome() {
@@ -103,33 +105,34 @@ int main() {
 	//semáforo
 	sem_init(&shared_memory->mutex, 1, 1); //endereço do semaforo, 0- compartilhado entre threads e 1 - compartilhado entre processos, valor inicial do semáforo
 
-	srand(time(NULL));
 	int random;
 
 	for(i = 0; i < 7; i++) {
 		shared_memory->pids[i] = -1;
 	}
 
-	shared_memory->pids[0] = fork();
-	if(shared_memory->pids[0] > 0) shared_memory->pids[1] = fork();
-	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0) shared_memory->pids[2] = fork();
-	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0) shared_memory->pids[3] = fork();
-	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0) shared_memory->pids[4] = fork();
-	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0 && shared_memory->pids[4] > 0) shared_memory->pids[5] = fork();
-	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0 && shared_memory->pids[4] > 0 && shared_memory->pids[5]>0) shared_memory->pids[6] = fork();
+	shared_memory->pids[0] = fork(); //p1
+	if(shared_memory->pids[0] > 0) shared_memory->pids[1] = fork(); //p2
+	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0) shared_memory->pids[2] = fork(); //p3
+	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0) shared_memory->pids[3] = fork(); //p4
+	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0) shared_memory->pids[4] = fork(); //p5
+	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0 && shared_memory->pids[4] > 0) shared_memory->pids[5] = fork(); //p6
+	if(shared_memory->pids[0] > 0 && shared_memory->pids[1] > 0 && shared_memory->pids[2] > 0 && shared_memory->pids[3] > 0 && shared_memory->pids[4] > 0 && shared_memory->pids[5]>0) shared_memory->pids[6] = fork(); //p7
 
 	if(shared_memory->pids[0] == 0) { //p1
+		srand(1234);
 		for(;;) {
 			enfileira_f1();
 			sleep(1);
 		}
-	}
-	if(shared_memory->pids[1] == 0) { //p2
+	} else if(shared_memory->pids[1] == 0) { //p2
+		srand(4321);
  		for(;;) {
 			enfileira_f1();
 			sleep(1);
 		}
  	} else if(shared_memory->pids[2] == 0) { //p3
+ 		srand(1423);
  		for(;;) {
 			enfileira_f1();
 			sleep(1);
