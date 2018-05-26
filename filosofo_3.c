a85d85a1869a09d732139d9535fc8239bde6540f
Lucasjrt <lucasjrt@outlook.com>
2018-05-25 23:51:44 -0300

    Jantar do filosofo 1 e 2

--- filosofo_3.c
+++ filosofo_3.c
@@ -3,17 +3,20 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 void comer(int id);
 void pensar(int id);
 void filosofo(int *id);
 
 int garfos[5] = {0}, filosofos[5];
+sem_t mutex;
 
 int main() {
 	int i, id;
 	srand(time(NULL));
 	pthread_t threads[5];
+	sem_init(&mutex, 0, 1); //variável do semáforo, 0 para semáforo entre threads e 1 para semáforo entre processos, valor inicial
 	for(i = 0; i < 5; i++) {
 		filosofos[i] = i;
 		pthread_create(&threads[i], NULL, (void*) filosofo, &filosofos[i]);
@@ -29,12 +32,14 @@ void comer(int id) {
 	switch(id) {
 		case 0:
 			if(!garfos[0] && !garfos[4]) {
+				sem_wait(&mutex);
 				garfos[0] = 1;
 				garfos[4] = 1;
 				printf("Filosofo %d comendo por % d segundos\n", id, n);
 				sleep(n);
 				garfos[0] = 0;
 				garfos[4] = 0;
+				sem_post(&mutex);
 			} else {
 				if(garfos[0])
 					printf("Garfo direito do filosofo %d ocupado\n", id);
@@ -47,12 +52,14 @@ void comer(int id) {
 			break;
 		case 1:
 			if(!garfos[1] && !garfos[0]) {
+				sem_wait(&mutex);
 				garfos[1] = 1;
 				garfos[0] = 1;
 				printf("Filosofo %d comendo por % d segundos\n", id, n);
 				sleep(n);
 				garfos[1] = 0;
 				garfos[0] = 0;
+				sem_post(&mutex);
 			} else {
 				if(garfos[1])
 					printf("Garfo direito do filosofo %d ocupado\n", id);
@@ -65,12 +72,14 @@ void comer(int id) {
 			break;
 		case 2:
 			if(!garfos[2] && !garfos[1]) {
+				sem_wait(&mutex);
 				garfos[2] = 1;
 				garfos[1] = 1;
 				printf("Filosofo %d comendo por % d segundos\n", id, n);
 				sleep(n);
 				garfos[2] = 0;
 				garfos[1] = 0;
+				sem_post(&mutex);
 			} else {
 				if(garfos[2])
 					printf("Garfo direito do filosofo %d ocupado\n", id);
@@ -83,12 +92,14 @@ void comer(int id) {
 			break;
 		case 3:
 			if(!garfos[3] && !garfos[2]) {
+				sem_wait(&mutex);
 				garfos[3] = 1;
 				garfos[2] = 1;
 				printf("Filosofo %d comendo por % d segundos\n", id, n);
 				sleep(n);
 				garfos[3] = 0;
 				garfos[2] = 0;
+				sem_post(&mutex);
 			} else {
 				if(garfos[3])
 					printf("Garfo direito do filosofo %d ocupado\n", id);
@@ -101,12 +112,14 @@ void comer(int id) {
 			break;
 		case 4:
 			if(!garfos[4] && !garfos[3]) {
+				sem_wait(&mutex);
 				garfos[4] = 1;
 				garfos[3] = 1;
 				printf("Filosofo %d comendo por % d segundos\n", id, n);
 				sleep(n);
 				garfos[4] = 0;
 				garfos[3] = 0;
+				sem_post(&mutex);
 			} else {
 				if(garfos[4])
 					printf("Garfo direito do filosofo %d ocupado\n", id);
